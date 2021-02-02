178d9ddb868fb7992199101ca337b13d1d1d24f0
Carles <carles.gstocks@gmail.com>
2021-02-02 22:51:45 +0100

    Issue Fixed

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -13,13 +13,12 @@ export default function Landing() {
     const { clamp } = useContext(Utils);
 
     // Accelerometer string
-    //const [tilt, setTilt] = useSpring(() => ({ tilt: "perspective(600px) rotateX(0deg) rotateY(0deg)" /*, config: { mass: 5, tension: 350, friction: 40 } */ }));
-    const [{ tilt }, setTilt] = useSpring(() => ({ tilt: "perspective(600px) rotateX(0deg) rotateY(0deg)" }));
+    const [{ tilt }, setTilt] = useSpring(() => ({ tilt: "perspective(1500px) rotateX(0deg) rotateY(0deg)" }));
 
     // Handle device orientation change
     const onOrientationChange = ({ gamma, beta }) => {
         console.log(gamma, beta);
-        setTilt({ tilt: `perspective(600px) rotateX(${clamp(beta * 0.5, -10, 10)}deg) rotateY(${clamp(-gamma * 0.5, -10, 10)}deg)` });
+        setTilt({ tilt: `perspective(1500px) rotateX(${clamp(beta * 0.5, -10, 10)}deg) rotateY(${clamp(-gamma * 0.5, -10, 10)}deg)` });
     };
 
     // Subscribe to events
